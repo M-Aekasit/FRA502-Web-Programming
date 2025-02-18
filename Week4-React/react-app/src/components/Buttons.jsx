d662d01718c083c87ddbe202d0adeea15e9922e9
aekasit <aekasit2002@gmail.com>
2025-02-19 03:54:44 +0700

    Update button labels in Buttons component for clarity

--- Week4-React/react-app/src/components/Buttons.jsx
+++ Week4-React/react-app/src/components/Buttons.jsx
@@ -4,13 +4,13 @@ const Buttons = ({ onIncrement, onDecrement, onReset, onSave }) => {
   return (
     <div className="card-btn">
       <button onClick={onDecrement} id="decrement">
-        -
+        Down
       </button>
       <button onClick={onReset} id="reset">
         Reset
       </button>
       <button onClick={onIncrement} id="increment">
-        +
+        Up
       </button>
       <button onClick={onSave} id="save">
         Save
