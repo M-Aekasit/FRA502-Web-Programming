2aefce3519ad1f67190043ec372847e3be1e35fa
aekasit <aekasit2002@gmail.com>
2025-03-03 23:34:52 +0700

    Add MenCounter and WomenCounter components for managing gender-specific counts

---
+++ Week5-Mysql/react-app/src/components/WomenCounter.jsx
@@ -0,0 +1,21 @@
+const WomenCounter = ({ count, setCount }) => {
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
+  return (
+    <div className="women-counter">
+      <h2>Women</h2>
+      <p>{count}</p>
+      <div className="button-container">
+        <button onClick={increment} className="button up-button">
+          UP
+        </button>
+        <button onClick={decrement} className="button down-button">
+          DOWN
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default WomenCounter;
